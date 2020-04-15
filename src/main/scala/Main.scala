eab61a30946787239017027a82e370e98f381f2e
n-aoki0409 <naoki19850409@gmail.com>
2020-04-15 22:23:21 +0900

    add scalafmt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,14 @@
-import org.stairwaybook.layout.Spiral
+import java.io.FileReader
 
-object Main extends App {
-  println("TEST OK")
-  println(Spiral.spiral(6, 0))
+import org.stairwaybook.parsing.JSON
+
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
+
+object Main extends App with JSON {
+  val reader = new FileReader(
+    "/Users/n-aoki/work/programming-in-scala/src/main/scala/org/stairwaybook/parsing/address-book.json")
+  println(parseAll(value, reader))
 }
--- src/main/scala/org/stairwaybook/parsing/JSON1.scala
+++ src/main/scala/org/stairwaybook/parsing/JSON1.scala
@@ -2,19 +2,20 @@ package org.stairwaybook.parsing
 
 import scala.util.parsing.combinator._
 
-class JSON1 extends JavaTokenParsers {
-  def obj: Parser[Map[String, Any]] = "{"~> repsep(member, ",") <~"}" ^^ (Map() ++ _)
-  def arr: Parser[List[Any]] = "["~> repsep(value, ",") <~"]"
-  def member: Parser[(String, Any)] = stringLiteral~":"~value ^^ {
-    case key~":"~value => (key, value)
+trait JSON1 extends JavaTokenParsers {
+  def obj: Parser[Map[String, Any]] =
+    "{" ~> repsep(member, ",") <~ "}" ^^ (Map() ++ _)
+  def arr: Parser[List[Any]] = "[" ~> repsep(value, ",") <~ "]"
+  def member: Parser[(String, Any)] = stringLiteral ~ ":" ~ value ^^ {
+    case key ~ ":" ~ value => (key, value)
   }
   def value: Parser[Any] = (
     obj
-  | arr
-  | stringLiteral
-  | floatingPointNumber ^^ (_.toDouble)
-  | "null" ^^ (_ => null)
-  | "true" ^^ (_ => true)
-  | "false" ^^ (_ => false)
+      | arr
+      | stringLiteral
+      | floatingPointNumber ^^ (_.toDouble)
+      | "null" ^^ (_ => null)
+      | "true" ^^ (_ => true)
+      | "false" ^^ (_ => false)
   )
 }
